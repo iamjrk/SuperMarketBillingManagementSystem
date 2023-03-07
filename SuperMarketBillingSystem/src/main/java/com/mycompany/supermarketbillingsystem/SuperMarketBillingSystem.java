64629c52b7fa9c40c6d8f1281486a87d10a4ba7d
Jyoti Ranjan Kumar <89156322+iamjrk@users.noreply.github.com>
2023-03-07 19:59:31 +0530

    Add files via upload

---
+++ SuperMarketBillingSystem/src/main/java/com/mycompany/supermarketbillingsystem/SuperMarketBillingSystem.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.supermarketbillingsystem;
+
+/**
+ *
+ * @author Sting
+ */
+public class SuperMarketBillingSystem {
+
+    public static void main(String[] args) {
+        new Opening();
+    }
+}
